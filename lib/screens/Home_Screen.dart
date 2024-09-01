25a7b3b862ec07fd8c304d55d38409d202a04e34
OmarAhmed342 <omaeahmad4000@gmail.com>
2024-09-01 03:43:47 +0300

    my changes

--- lib/screens/Home_Screen.dart
+++ lib/screens/Home_Screen.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/widgets/app_double_text.dart';
+
+import '../base/res/media.dart';
+import '../base/res/styles/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,8 +11,10 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: AppStyles.bgColor,
         body: ListView(
       children: [
+        const SizedBox(height: 40,),
         Container(
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
@@ -16,35 +22,48 @@ class HomeScreen extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Column(
+                  Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        "Good Morning",
-                        style: TextStyle(
-                            fontSize: 17, fontWeight: FontWeight.w500),
-                      ),
-                      SizedBox(
+                      Text("Good Morning", style: AppStyles.headLineStyle3),
+                      const SizedBox(
                         height: 5,
                       ),
-                      Text(
-                        "Book Tickets",
-                        style: TextStyle(
-                            fontSize: 26,
-                            fontWeight: FontWeight.w500,
-                            color: Color(0xFF3b3b3b)),
-                      ),
+                      Text("Book Tickets", style: AppStyles.headLineStyle1),
                     ],
                   ),
-                  Container()
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                            image: AssetImage(AppMedia.logo))),
+                  )
                 ],
               ),
-              const Row(
-                children: [Text("search"), Text("Space")],
-              )
+              const SizedBox(height: 25,),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 12),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD),
+                ),
+                child: const Row(
+                  children: [
+                    Icon(FluentSystemIcons.ic_fluent_search_regular,
+                        color: Color(0xFFBFC205)),
+                    Text("search")
+                  ],
+                ),
+              ),
+              const SizedBox(height: 40,),
+              const AppDoubleText(bigText: "Upcoming Flights", smallText: "View all"),
             ],
+
           ),
         ),
+
       ],
     ));
   }
